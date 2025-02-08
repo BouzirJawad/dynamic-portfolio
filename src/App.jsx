6f23ab0add73b16b2bf00ce40587a7dc39e1b633
BouzirJawad <jawadbouzir01@gmail.com>
2025-02-08 14:07:55 +0100

    Added home section

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
-import Header from "./Components/Header";
-import Footer from "./Components/Footer";
+import Header from "./Components/Header"
+import Home from "./Components/Home"
+import Footer from "./Components/Footer"
 
 
 function App() {
@@ -8,7 +9,7 @@ function App() {
     <div className="flex flex-col min-h-screen">
       <Header />
       <main className="grow mt-25">
-
+        <Home />
       </main>
       <Footer />
     </div>
---
+++ src/Components/Home.jsx
@@ -0,0 +1,22 @@
+import chillGuy from '../assets/95c.png'
+
+function Home() {
+    
+    return(
+        <section className="w-[95%] sm:w-[75%] md:w-[55%] mx-auto rounded-xl mainDiv">
+            <div className="w-full flex flex-wrap justify-center items-center p-3">
+                <div className="w-1/3 md:w-1/2 sm:max-w-sm">
+                    <img src={chillGuy} alt="chill guy" />
+                </div>
+                <div className="w-2/3 sm:w-1/2 mt-3 lg:space-y-5 xl:space-y-7">
+                    <p className="text-2xl font-bold">Hello,</p>
+                    <p className="text-xl font-bold">I'm Bouzir Jawad</p>
+                    <p className="text-lg">full stack developer / freelancer</p>
+                    <button className="primary-btn mb-2 inline-block">Get in touch</button>
+                </div>
+            </div>
+        </section>
+    );
+}
+
+export default Home
